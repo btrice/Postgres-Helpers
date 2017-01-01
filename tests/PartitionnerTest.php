d7df0bc49848f17853656044a8fe17ed7cfeec0a
Aidara Moussa <aidara84@yahoo.fr>
2017-01-01 11:52:12 +0100

    Add build tests linked to Travis CI

---
+++ tests/PartitionnerTest.php
@@ -0,0 +1,9 @@
+ <?php 
+  require dirname(__FILE__) . '/../Partitionner.php';
+ // parent_table_name, check_field, start_date, number of child table, months step
+ $chiltable = new Partionner("projection","start_datetime","2017-01-01",4,1);
+/* $chiltable->disable_Trigger();
+ $chiltable->disable_Index();
+ $chiltable->disable_Trigger_function();*/
+ $chiltable->generate_child_table();
+ ?>
