43963c6ff2430c79f18d16836ecd3b49fea16bfa
Aidara Moussa <msaidara@gmail.com>
2017-01-01 10:56:00 +0100

    Update Partionner.php
    Suppression des tests

--- Partionner.php
+++ Partionner.php
@@ -124,11 +124,3 @@ class Partionner{
 
         }
 }
-
- // parent_table_name, check_field, start_date, number of child table, months step
- $chiltable = new Partionner("projection","start_datetime","2015-01-01",4,1);
-/* $chiltable->disable_Trigger();
- $chiltable->disable_Index();
- $chiltable->disable_Trigger_function();*/
- $chiltable->generate_child_table();
- 
