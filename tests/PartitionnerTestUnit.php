fe43dff6f724195174dff50c41e87ad8e79b8921
Aidara Moussa <msaidara@gmail.com>
2017-01-02 08:48:25 +0100

    add unit test

--- tests/PartitionnerTestUnit.php
+++ tests/PartitionnerTestUnit.php
@@ -6,12 +6,12 @@ class PartitionnerTestUnit extends PHPUnit_Framework_TestCase
 	public function setUp()
 	{
 		$this->chiltable = new Partionner("projection","start_datetime","2017-01-01",4,1);
-		$this->assertNull(error_get_last());
+		$this->assertTrue(isset($this->chiltable));
 	}
 	public function testGenerate()
     {
 		$this->chiltable->generate_child_table();
-		$this->assertNull(error_get_last());
+		$this->assertTrue(isset($this->chiltable));
 	}
 
 }
