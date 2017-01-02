1eed2a39b328591edab8b166ec3160b48d44d47b
Aidara Moussa <msaidara@gmail.com>
2017-01-02 08:54:47 +0100

    Move unit test from partitionnerTestUnit
    Move unit test from partitionnerTestUnit to partitionnerTest

--- tests/PartitionnerTest.php
+++ tests/PartitionnerTest.php
@@ -1,10 +1,17 @@
- #!/usr/bin/env php
  <?php 
-  require dirname(__FILE__) . '/../Partitionner.php';
- // parent_table_name, check_field, start_date, number of child table, months step
- $chiltable = new Partionner("projection","start_datetime","2017-01-01",4,1);
-/* $chiltable->disable_Trigger();
- $chiltable->disable_Index();
- $chiltable->disable_Trigger_function();*/
- $chiltable->generate_child_table();
+class PartitionnerTestUnit extends PHPUnit_Framework_TestCase
+{ 
+	 private $chiltable;
+ 
+	 public function setUp()
+	{
+		$this->chiltable = new Partionner("projection","start_datetime","2017-01-01",4,1);
+		$this->assertTrue(isset($this->chiltable));
+	}
+	public function testGenerate()
+    {
+		$this->chiltable->generate_child_table();
+		$this->assertTrue(isset($this->chiltable));
+	}
+}
  ?>
