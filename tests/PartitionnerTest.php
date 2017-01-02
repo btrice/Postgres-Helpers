30a6f899062fda9f4756cb1378e9c0c4d6bf1005
Aidara Moussa <msaidara@gmail.com>
2017-01-02 08:57:43 +0100

    change class name

--- tests/PartitionnerTest.php
+++ tests/PartitionnerTest.php
@@ -5,7 +5,7 @@ class PartitionnerTestUnit extends PHPUnit_Framework_TestCase
  
 	 public function setUp()
 	{
-		$this->chiltable = new Partionner("projection","start_datetime","2017-01-01",4,1);
+		$this->chiltable = new Partitionner("projection","start_datetime","2017-01-01",4,1);
 		$this->assertTrue(isset($this->chiltable));
 	}
 	public function testGenerate()
